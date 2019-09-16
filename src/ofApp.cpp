b8077bb81e3ca566dac4cb93ae480434cd1e4501
skiprox <seanmichaelscanlan@gmail.com>
2019-09-16 13:12:31 -0500

    Updating image

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -60,16 +60,21 @@ void ofApp::setupAnimations(){
 	pts.push_back(array<glm::vec2, 2> {{glm::vec2(1370, 840), glm::vec2(fixedWidth/2.0, 840)}});
 	companiesLine = DashedLine(pts, 5.0, red, 250, false);
 	pts.clear();
-	// Press points for everyone
-	pressPts.push_back(array<glm::vec2, 2> {{glm::vec2(800, 700), glm::vec2(970, 635)}});
-	peoplePressLine = DottedLine(pressPts, 5.0, red, 75, true);
-	pressPts.clear();
 	// Aerial to upper building dashed lines
 	pts.push_back(array<glm::vec2, 2> {{glm::vec2(780, 375), glm::vec2(1350, 375)}});
 	pts.push_back(array<glm::vec2, 2> {{glm::vec2(1350, 375), glm::vec2(1350, 275)}});
 	pts.push_back(array<glm::vec2, 2> {{glm::vec2(1350, 275), glm::vec2(1555, 275)}});
 	aerialToBuildingLine = DashedLine(pts, 2.0, red, 250, true);
 	pts.clear();
+	/**
+	 * ALL THE PRESS POINTS
+	 */
+	// 0, millenium right
+	// 9, people
+	pressPts.push_back(array<glm::vec2, 2> {{glm::vec2(800, 700), glm::vec2(970, 635)}});
+	pressLines.push_back(DottedLine(pressPts, 5.0, red, 75, true));
+	peoplePressLine = DottedLine(pressPts, 5.0, red, 75, true);
+	pressPts.clear();
 	/**
 	 * ALL THE IMAGE ANIMATIONS GO HERE
 	 */
@@ -464,8 +469,8 @@ void ofApp::runAnimation(int animationNum){
 			if (!shouldRunAnimation[9]) {
 				companiesCenterText.update(animationCounter[4]);
 				companiesCenterText.draw();
-				pressLine[9].update(animationCounter[4]);
-				pressLine[9].draw();
+				// pressLine[9].update(animationCounter[4]);
+				// pressLine[9].draw();
 				handMarkers[9].draw();
 			}
 			ofPopStyle();
